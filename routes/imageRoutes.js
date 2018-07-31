03ef7783b8857cc5054eb99c1c9cd49b1aa7536a
Mattlomet <mattlomet@live.com>
2018-07-31 10:19:32 -0400

    updated file structure and facial rec

--- routes/imageRoutes.js
+++
@@ -1,8 +0,0 @@
-var db = require("../models");
-var path = require("path");
-
-module.exports = function (app) {
-    app.get('/images',(req,res)=>{
-        res.sendFile(path.join(__dirname, "../public/facImage.html"));
-    })
-};
