1b4dcd706f0ba6567a335dc93a452dd5cca69ffb
erin0418 <erinf0418@gmail.com>
2018-07-31 23:46:25 -0400

    html updated

--- data/personality.js
+++ data/personality.js
@@ -1,27 +1,20 @@
+var PersonalityInsightsV3 = require("watson-developer-cloud/personality-insights/v3");
 var authPersonality = require("../keys/personalityKey");
 var tweets =
   "Hello, this is a sample tweet that will be replaced with Twitter data that will come in from the Twitter login requirements. This will be the text that will be pushed through the personality API by Watson. Hello, this is a sample tweet that will be replaced with Twitter data that will come in from the Twitter login requirements. This will be the text that will be pushed through the personality API by Watson. Hello, this is a sample tweet that will be replaced with Twitter data that will come in from the Twitter login requirements. This will be the text that will be pushed through the personality API by Watson. Hello, this is a sample tweet that will be replaced with Twitter data that will come in from the Twitter login requirements. This will be the text that will be pushed through the personality API by Watson.";
-var results;
 
-var PersonalityInsightsV3 = require("watson-developer-cloud/personality-insights/v3");
-var personalityInsights = new PersonalityInsightsV3({
-  version: authPersonality.version,
-  username: authPersonality.username,
-  password: authPersonality.password
-});
-var profileParams = {
-  content: tweets,
-  content_type: "text/plain",
-  consumption_preferences: true,
-  raw_scores: true
-};
-personalityInsights.profile(profileParams, function(error, profile) {
-  if (error) {
-    console.log(error);
-  } else {
-    JSON.stringify(profile, null, 2);
-    results = profile;
+var personality = {
+  personalityInsights: new PersonalityInsightsV3({
+    version: authPersonality.version,
+    username: authPersonality.username,
+    password: authPersonality.password
+  }),
+  profileParams: {
+    content: tweets,
+    content_type: "text/plain",
+    consumption_preferences: true,
+    raw_scores: true
   }
-});
+};
 
-module.exports = results;
+module.exports = personality;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,18 @@
-var dataPersonality = require("../data/personality.js");
+var Personality = require("../data/personality.js");
 var dataTone = require("../data/tone.js");
 
-console.log(dataPersonality);
-
 module.exports = function(app) {
-  app.get("api/personality", function(req, res) {
-    res.send(dataPersonality);
+  app.post("api/personality", function(req, res) {
+    Personality.personalityInsights.profile(Personality.profileParams, function(
+      error,
+      profile
+    ) {
+      if (error) {
+        console.log(error);
+      } else {
+        res.send(JSON.stringify(profile, null, 2));
+      }
+    });
   });
 
   app.post("api/tone", function(req, res) {
