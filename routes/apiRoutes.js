770bf9ae60f4266dc21aa945b1e9f77184ec0652
Mattlomet <mattlomet@live.com>
2018-08-02 14:45:43 -0400

    completed personality backend

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,41 @@
 var Personality = require("../data/personality.js");
+console.log(Personality);
+var twitterContent = require("../data/twitter.js");
+
+
 var Tone = require("../data/tone.js");
 // var personality = require("../data/personalityArray");
 var tone = require("../data/toneArray");
+var hello = "hello";
+
 module.exports = function(app,personalityData) {
   // console.log(app);
-  // console.log(personalityData);
-  app.get("api/personality", function (req,res){
-    console.log(req);
-    console.log(personalityData);
+   console.log(personalityData);
 
-    res.json(personalityData);
+
+  app.get("/twitterName", function (req,res){
+    // console.log(req)
+    
   })
-  app.get("api/tone", function (req,res){
+  app.get("/api/tone", function (req,res){
     res.json(tone);
   })
-  app.post("api/personality", function(req, res) {
+  app.post("/api/personality", function(req, res) {
     personality.push(Personality.result);
   });
 
-  app.post("api/tone", function(req, res) {
+  app.post("/api/tone", function(req, res) {
     tone.push(Tone.result);
   });
   app.post('/twitterName',(req,res)=>{
     console.log(req.body.name);
     var screenName = req.body.name;
-    require('../data/twitter')(app,screenName);
-
+    twitterContent(screenName)
+    .then(function(content){
+      Personality(content)
+      .then(function(analysis){
+        res.json(analysis);
+      })
+    });
   })
 };
