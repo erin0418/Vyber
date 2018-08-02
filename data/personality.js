da33797c45096696120efe2f2d7d56bf9e659c33
erin0418 <erinf0418@gmail.com>
2018-08-01 22:40:14 -0400

    waiting for twitter inforamtion

--- data/personality.js
+++ data/personality.js
@@ -1,20 +1,31 @@
+// Require twitter information
+var Tweets = require("")
+
 var PersonalityInsightsV3 = require("watson-developer-cloud/personality-insights/v3");
 var authPersonality = require("../keys/personalityKey");
-var tweets =
-  "Hello, this is a sample tweet that will be replaced with Twitter data that will come in from the Twitter login requirements. This will be the text that will be pushed through the personality API by Watson. Hello, this is a sample tweet that will be replaced with Twitter data that will come in from the Twitter login requirements. This will be the text that will be pushed through the personality API by Watson. Hello, this is a sample tweet that will be replaced with Twitter data that will come in from the Twitter login requirements. This will be the text that will be pushed through the personality API by Watson. Hello, this is a sample tweet that will be replaced with Twitter data that will come in from the Twitter login requirements. This will be the text that will be pushed through the personality API by Watson.";
 
-var personality = {
-  personalityInsights: new PersonalityInsightsV3({
+var personalityInsights = new PersonalityInsightsV3({
     version: authPersonality.version,
     username: authPersonality.username,
     password: authPersonality.password
   }),
-  profileParams: {
-    content: tweets,
+var profileParams = {
+    // Add in tweets.() to call all tweets
+    content: Tweets.text,
     content_type: "text/plain",
     consumption_preferences: true,
     raw_scores: true
   }
-};
 
-module.exports = personality;
+  personalityInsights.profile(profileParams, function (error,analysis){
+    if(err){
+      console.log(error);
+    }
+    else {
+      analysis
+    }
+  })
+
+module.exports = {
+  result: analysis
+}
--- data/tone.js
+++ data/tone.js
@@ -1,3 +1,5 @@
+// var Text = require("../public/tone")
+
 var authTone = require("../keys/toneKey");
 
 var ToneAnalyzerV3 = require("watson-developer-cloud/tone-analyzer/v3");
@@ -9,8 +11,7 @@ var toneAnalyzer = new ToneAnalyzerV3({
   username: authTone.username,
   password: authTone.password
 });
-var text =
-  "Hello, this is a sample tweet that will be replaced with Twitter data that will come in from the Twitter login requirements. This will be the text that will be pushed through the personality API by Watson. Hello, this is a sample tweet that will be replaced with Twitter data that will come in from the Twitter login requirements. This will be the text that will be pushed through the personality API by Watson. Hello, this is a sample tweet that will be replaced with Twitter data that will come in from the Twitter login requirements. This will be the text that will be pushed through the personality API by Watson. Hello, this is a sample tweet that will be replaced with Twitter data that will come in from the Twitter login requirements. This will be the text that will be pushed through the personality API by Watson.";
+var text = req.body;
 
 var toneParams = {
   tone_input: { text: text },
@@ -27,4 +28,8 @@ toneAnalyzer.tone(toneParams, function(error, analysis) {
 });
 0;
 
-module.exports = result;
+module.exports = {
+  result: analysis,
+}
+// app.js
+
