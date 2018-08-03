476073147163cf806b15e9aaf3cacd0020ef2121
Mattlomet <mattlomet@live.com>
2018-08-02 21:05:05 -0400

    working tone backing logic (console.log(data) in the front end for tone)

--- data/tone.js
+++ data/tone.js
@@ -1,33 +1,40 @@
 // var Text = require("../public/tone")
+module.exports = function (Tweets) {
+  return new Promise((resolve, reject) => {
 
-var authTone = require("../keys/toneKey");
 
-var ToneAnalyzerV3 = require("watson-developer-cloud/tone-analyzer/v3");
+    var authTone = require("../keys/toneKey");
 
-var analysis;
+    var ToneAnalyzerV3 = require("watson-developer-cloud/tone-analyzer/v3");
 
-var toneAnalyzer = new ToneAnalyzerV3({
-  version: authTone.version,
-  username: authTone.username,
-  password: authTone.password
-});
-var text = "Hello, this is a sample text. Hello, this is a sample text. Hello, this is a sample text. Hello, this is a sample text. Hello, this is a sample text.Hello, this is a sample text. Hello, this is a sample text. Hello, this is a sample text. Hello, this is a sample text. Hello, this is a sample text. Hello, this is a sample text. Hello, this is a sample text. Hello, this is a sample text. Hello, this is a sample text. Hello, this is a sample text.";
 
-var toneParams = {
-  tone_input: { text: text },
-  content_type: "application/json"
-};
+    var toneAnalyzer = new ToneAnalyzerV3({
+      version: authTone.version,
+      username: authTone.username,
+      password: authTone.password
+    });
+    var utterances = []
+    for (var i = 0; i < Tweets.contentItems.length; i++) {
+      var tweetObject = {
+        "text":Tweets.contentItems[i].content,
+        "user":Tweets.contentItems[i].id
+      };
+      
+      utterances.push(tweetObject);
 
-toneAnalyzer.tone(toneParams, function(error, analysis) {
-  if (error) {
-    console.log(error);
-  } else {
-    analysis;
-  }
-});
-0;
+    };
 
-module.exports = {
-  result: analysis,
+    var toneChatParams = {
+      utterances: utterances
+    };
+
+    toneAnalyzer.toneChat(toneChatParams, function (error, analysis) {
+      if (error) {
+        console.log(error);
+      } else {
+        resolve(analysis);
+      }
+    });
+  });
+
 }
-
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,27 @@
 var Personality = require("../data/personality.js");
-console.log(Personality);
-var twitterContent = require("../data/twitter.js");
-
-
 var Tone = require("../data/tone.js");
-// var personality = require("../data/personalityArray");
-var tone = require("../data/toneArray");
-var hello = "hello";
+var twitterContent = require("../data/twitter.js");
+var Tone = require("../data/tone.js");
+module.exports = function(app) {
 
-module.exports = function(app,personalityData) {
-  // console.log(app);
-   console.log(personalityData);
-
-
-  app.get("/twitterName", function (req,res){
-    // console.log(req)
+  // app.get("/twitterName", function (req,res){
+  //   // console.log(req)
     
-  })
-  app.get("/api/tone", function (req,res){
-    res.json(tone);
-  })
-  app.post("/api/personality", function(req, res) {
-    personality.push(Personality.result);
-  });
+  // })
+  // app.get("/api/tone", function (req,res){
+  //   res.json(tone);
+  // })
+  // app.post("/api/personality", function(req, res) {
+  //   personality.push(Personality.result);
+  // });
 
-  app.post("/api/tone", function(req, res) {
-    tone.push(Tone.result);
-  });
+  // app.post("/api/tone", function(req, res) {
+  //   tone.push(Tone.result);
+  // });
   app.post('/twitterName',(req,res)=>{
     console.log(req.body.name);
     var screenName = req.body.name;
+    
     twitterContent(screenName)
     .then(function(content){
       Personality(content)
@@ -37,5 +29,14 @@ module.exports = function(app,personalityData) {
         res.json(analysis);
       })
     });
+  })
+  app.post('/ToneRoute', (req, res) => {
+    var screenName = req.body.name;
+    twitterContent(screenName)
+      .then(function (content) {
+        Tone(content).then(function (analysis) {
+            res.json(analysis);
+          })
+      });
   })
 };
