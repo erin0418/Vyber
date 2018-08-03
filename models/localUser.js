5cf7be60f6880175a856e2844965f2cd7d2d10cf
Mattlomet <mattlomet@live.com>
2018-08-03 09:18:50 -0400

    heroku backend database

--- models/localUser.js
+++ models/localUser.js
@@ -1,5 +1,5 @@
 module.exports = function (sequelize, DataTypes) {
-    var localUser = sequelize.define("localuser", {
+    var localUser = sequelize.define("localUser", {
         username: {
             type: DataTypes.STRING,
             allowNull: false,
