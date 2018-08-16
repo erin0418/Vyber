f7855843bd0f6031abea69af7d7d3af6053568a0
Mattlomet <mattlomet@live.com>
2018-08-16 13:38:24 -0400

    fixed routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from "react";
-import "./App.css";
-import User from "./components/userTest/user";
-import Form from "./components/formTest/form";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from './components/navbar/navbar';
+import SignIn from "./components/signin/signin";
+import Home from "./components/home/home";
+import Manage from "./components/manage/manage";
+import Search from "./components/search/search";
+import Tone from "./components/tone/tone";
+import Personality from "./components/personality/personality";
+import FaceReg from "./components/faceReq/face";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="usersFromDataBase">
-          <User />
-        </div>
-        <div className="formsCallingOurRoutes">
-          <Form />
-        </div>
-      </div>
+      <Router>
+        <div>
+          <NavBar/>
+          <Route exact path="/" component={SignIn}/>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/manage" component={Manage} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/tone" component={Tone} />
+          <Route exact path="/personality" component={Personality} />
+          <Route exact path="/face" component={FaceReg} />
+          </div>
+      </Router>
     );
   }
 }
