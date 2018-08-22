c400c4a84100e3f5e25d82cdd3de9ce9657b2c35
Syreeta Monroe <syreeta.monroe@gmail.com>
2018-08-22 11:08:52 -0400

    login updated

--- client/src/components/signin/children/login.js
+++ client/src/components/signin/children/login.js
@@ -3,9 +3,15 @@ import { Link } from "react-router-dom";
 
 const LogIn = props => (
   <div className="toneRouteForm">
+    <div className="input-field">
     <input name="username" type="text" onChange={props.handleInputChange} />
+    <label for="username" className="white-text" id ="p1">User Name</label>
+    </div>
+    <div className="input-field">
     <input name="password" type="text" onChange={props.handleInputChange} />
-    <button onClick={props.handleAccountApp}>
+    <label for="username" className="white-text" id ="p1">Password</label>
+    </div>
+    <button class="submit btn amber lighten-5 waves-effect waves-orange"  onClick={props.handleAccountApp}>
       <Link to="/home"> Sign In </Link>
     </button>
   </div>
