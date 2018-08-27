b1b54831c0b0db782747c5e2cd86ef9c6c33f22e
Ray <rkilburg42@gmail.com>
2018-08-27 12:43:49 -0400

    tonefinish

--- client/src/components/tone/children/tonedisplay.js
+++ client/src/components/tone/children/tonedisplay.js
@@ -4,48 +4,29 @@ import "./tonedisplay.css";
 
 const Display = props => (
   <div>
-<<<<<<< HEAD
-
-      
         <div>
           <div className="container">
           <div className="col s12 m7">
             {/* <h2 className="header">Tweet:</h2> */}
             <div className="card-stacked" id="card">
               <div className="card-content black-text">
-                <p id="cardtext"> {props.results} </p>
+                <p id="cardtext"> {props.results.text} </p>
               </div>
-              <div className="card-action">
+              {/* <div className="card-action">
+            </div> */}
             </div>
-            </div>
-              <div className="chip">
-                Tone
-              </div>
+              
+                {props.results.tone.map(result => (
+                  <div className="chip">
+                     <p className="flow-text">{result}</p>
+                  </div>
+                ))}
+
+                {/* <hr></hr> */}
+              
           </div>
           </div>
         </div> 
-  
-
-=======
-    {props.results.utterances_tone.map(result => {
-      console.log(result);
-      return (
-        <div>
-          <div class="col s12 m7">
-            <h2 class="header">Horizontal Card</h2>
-            <div class="card-stacked">
-              <div class="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information.</p>
-              </div>
-              <div class="card-action">
-                <a href="#">This is a link</a>
-              </div>
-            </div>
-          </div>
-        </div>
-  )
-})}
->>>>>>> e7ef0a44b5afd579ba4f97155b747533500b2b23
   </div>
 );
 export default Display;
--- client/src/components/tone/tone.js
+++ client/src/components/tone/tone.js
@@ -34,33 +34,70 @@ export default class Tone extends Component {
       })
       .then(body => {
         console.log(body.utterances_tone);
-        let textArray = []
+        let textArray = [];
+        
+        let resArray = [];
         for (let i =0; i< body.utterances_tone.length; i++){
           textArray.push(body.utterances_tone[i].utterance_text)
-          // textArray.push(body.utterance_tone[i].tones)
+          let toneArray = [];
+          
+          for(let j =0; j< body.utterances_tone[i].tones.length; j++){
+            //console.log(body.utterances_tone[i].tones.length)
+            //console.log(body.utterances_tone[i].tones[j].tone_name)
+            if(body.utterances_tone[i].tones[j].tone_name){
+              toneArray.push(body.utterances_tone[i].tones[j].tone_name)
+              console.log(toneArray)
+            }
+          }
+          // textArray.push(body.utterance_tone[i].tones[0].tone_name)
+
+          resArray.push({
+            text: body.utterances_tone[i].utterance_text,
+            tone: toneArray
+          })
+         
         }
+        console.log(resArray)
         this.setState({
-          results: textArray,
+          results: resArray,
           hideInputs: true
         });
       });
   };
 
-
   render() {
     return (
       <div>
-        
-          {this.state.hideInputs === false}
+        {/* hide modal */}
+        {this.state.hideInputs === false ? (
           <UserInputs
-             handlePage={this.handlePage} 
-             handleInput={this.handleInput}
-          /> 
-        {this.state.results.map(result => (
-           <Display results={result}/>
-          ))}
-      
+            handlePage={this.handlePage}
+            handleInput={this.handleInput}
+          />
+        ) : (
+          // <Display results={this.state.results} />
+          this.state.results.map(result => (
+                    <Display results={result}/>
+                     ))
+        )}
       </div>
-    )
-  };
+    );
+  }
+  // with modal
+  // render() {
+  //   return (
+  //     <div>
+        
+  //         {this.state.hideInputs === false}
+  //         <UserInputs
+  //            handlePage={this.handlePage} 
+  //            handleInput={this.handleInput}
+  //         /> 
+  //       {this.state.results.map(result => (
+  //          <Display results={result}/>
+  //         ))}
+      
+  //     </div>
+  //   )
+  // };
 }
